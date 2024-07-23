b7f5467c93f95fb6c8004f784f7c78c44940618b
farhan <farhanj.mohamed@gmail.com>
2024-07-22 18:12:59 -0700

    links and skills imgs

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import ppic from "./assets/ppic.png";
+import rust from "./assets/skills/rust.png";
+import csharp from "./assets/skills/csharp.png";
+import bootstrap from "./assets/skills/bootstrap.png";
+import ruby from "./assets/skills/ruby.png";
+import js from "./assets/skills/js.png";
+import html from "./assets/skills/html.png";
+import css from "./assets/skills/css.png";
+import react from "./assets/skills/react.png";
+import rails from "./assets/skills/rails.png";
+import postgres from "./assets/skills/postgres.png";
+import tailwindcss from "./assets/skills/tailwindcss.png";
+import npm from "./assets/skills/npm.png";
+import git from "./assets/skills/git.png";
+import cargo from "./assets/skills/cargo.png";
+import linkedin from "./assets/links/linkedin.png";
+import github from "./assets/links/github.png";
+import calendly from "./assets/links/calendly.png";
 
 function App() {
   return (
-    <div className="h-auto min-h-screen w-screen bg-gradient-to-b from-zinc-900 to-zinc-700 pb-20">
+    <div className="h-auto min-h-screen w-screen bg-gradient-to-b from-zinc-900 to-zinc-700 ">
       <p className="text-center font-black text-5xl text-gray-50 pt-20">Farhan Mohamed</p>
 
       <div className=" w-2/3 mx-auto mt-2 h-auto" id="profile-pic">
@@ -10,12 +27,23 @@ function App() {
       </div>
 
       <p className="text-center font-black text-2xl text-gray-50 pt-20">About</p>
-      <div className="w-2/3 border-gray-100 border rounded-xl mx-auto mt-2 h-auto" id="about">
-        <p className="p-5 text-white">
+      <div className="w-1/2 border-gray-100 border rounded-xl mx-auto mt-2 h-auto" id="about">
+        <p className="w-1/2 text-center mx-auto p-5 text-white">
           I'm eager to bring my imaginative flair to the tech world and play a part in shaping its future. I'm seeking a
           role in tech where I can learn and make an impact. I am looking for a growth-oriented environment where I can
           develop innovative ideas.
         </p>
+        <div className=" flex flex-row justify-center gap-5 pb-5" id="links">
+          <a href="https://www.linkedin.com/in/farhanjmohamed/">
+            <img title="Linkedin" src={linkedin} className="h-10 w-auto" />
+          </a>
+          <a href="https://github.com/farhanjmohamed">
+            <img title="Github" src={github} className="h-10 w-auto" />
+          </a>
+          <a href="https://calendly.com/farhanjmohamed">
+            <img title="Calendly" src={calendly} className="h-10 w-auto" />
+          </a>
+        </div>
       </div>
 
       <p className="text-center font-black text-2xl text-gray-50 pt-20">Skills</p>
@@ -23,65 +51,112 @@ function App() {
         <div className="flex justify-center">
           <div className="text-center grid grid-cols-2 gap-40">
             <ol>
-              <li>Rust</li>
-              <li>Ruby</li>
-              <li>Javascript</li>
-              <li>C#</li>
-              <li>HTML</li>
-              <li>CSS</li>
-              <li>PostgresSQL</li>
+              <li className="flex flex-row justify-start items-center gap-5">
+                <img src={rust} className="w-auto h-10" />
+                <p>Rust </p>
+              </li>
+              <li className="flex flex-row justify-start items-center gap-5">
+                <img src={ruby} className="w-auto h-10" />
+                <p>Ruby</p>
+              </li>
+              <li className="flex flex-row justify-start items-center gap-5">
+                <img src={js} className="w-auto h-10" />
+                <p>Javascript</p>
+              </li>
+              <li className="flex flex-row justify-start items-center gap-5">
+                C# <img src={csharp} className="w-auto h-10" />
+              </li>
+              <li className="flex flex-row justify-start items-center gap-5">
+                HTML <img src={html} className="w-auto h-10" />
+              </li>
+              <li className="flex flex-row justify-start items-center gap-5">
+                CSS <img src={css} className="w-auto h-10" />
+              </li>
+              <li className="flex flex-row justify-start items-center gap-5">
+                PostgresSQL <img src={postgres} className="w-auto h-10" />
+              </li>
             </ol>
             <ol>
-              <li>Ruby on Rails</li>
-              <li>Reactjs</li>
-              <li>Tailwindcss</li>
-              <li>Bootstrap</li>
-              <li>Git</li>
-              <li>Github</li>
-              <li>Unity</li>
+              <li className="flex flex-row justify-start items-center gap-5">
+                Ruby on Rails <img src={rails} className="w-auto h-10" />
+              </li>
+              <li className="flex flex-row justify-start items-center gap-5">
+                Reactjs <img src={react} className="w-auto h-10" />
+              </li>
+              <li className="flex flex-row justify-start items-center gap-5">
+                Tailwindcss <img src={tailwindcss} className="w-auto h-10" />
+              </li>
+              <li className="flex flex-row justify-start items-center gap-5">
+                Bootstrap <img src={bootstrap} className="w-auto h-10" />
+              </li>
+              <li className="flex flex-row justify-start items-center gap-5">
+                Git <img src={git} className="w-auto h-10" />
+              </li>
+              <li className="flex flex-row justify-start items-center gap-5">
+                Npm <img src={npm} className="w-auto h-10" />
+              </li>
+              <li className="flex flex-row justify-start items-center gap-5">
+                Cargo <img src={cargo} className="w-auto h-10" />
+              </li>
             </ol>
           </div>
         </div>
       </div>
 
       <p className="text-center font-black text-2xl text-gray-50 pt-20">Projects</p>
-      <div className="w-2/3 pb-10 border-gray-100 border-2 mx-auto mt-2 h-auto" id="projects">
-        <p className="text-center font-bold text-xl text-white pt-5">Web Dev</p>
-        <div className="flex flex-col text-white">
-          <div id="project-block" className="text-left w-1/2 pl-5">
-            <p>Fittr</p>
-            <p>
+      <div className="w-2/3 pb-10 border-gray-100 border-2 mx-auto mt-2 mb-10 h-auto" id="projects">
+        <p className="text-center font-bold text-xl text-white pt-5 pb-5">Web Dev</p>
+        <div className="flex flex-col text-white justify-center items-center">
+          <div id="project-block" className="text-left w-1/2 p-4 border border-white rounded-xl ml-5 mb-5">
+            <p className="pb-2 text-lg">Fittr</p>
+            <p className="pb-2">
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur similique autem, laboriosam iste ad
               enim velit corporis molestiae commodi quibusdam.
             </p>
-            <button className="border-2 border-white p-1 w-20 rounded-xl hover:bg-gray-300">Repo</button>
-            <button className="border-2 border-white p-1 w-20 rounded-xl hover:bg-gray-300">Demo</button>
+            <button className="border-2 border-white p-1 w-20 rounded-xl hover:bg-zinc-950 mr-2">Repo</button>
+            <button className="border-2 border-white p-1 w-20 rounded-xl hover:bg-zinc-950">Demo</button>
           </div>
-          <div id="project-block" className="text-left w-1/2 pl-5">
-            <p>JS Fun Box</p>
-            <p>
+          <div id="project-block" className="text-left w-1/2 p-4 border border-white rounded-xl ml-5 mb-5">
+            <p className="pb-2 text-lg">JS Fun Box</p>
+            <p className="pb-2">
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur similique autem, laboriosam iste ad
               enim velit corporis molestiae commodi quibusdam.
             </p>
-            <button className="border-2 border-white p-1 w-20 rounded-xl hover:bg-gray-300">Repo</button>
-            <button className="border-2 border-white p-1 w-20 rounded-xl hover:bg-gray-300">Demo</button>
+            <button className="border-2 border-white p-1 w-20 rounded-xl hover:bg-zinc-950 mr-2">Repo</button>
+            <button className="border-2 border-white p-1 w-20 rounded-xl hover:bg-zinc-950">Demo</button>
           </div>
-          <div id="project-block" className="text-left w-1/2 pl-5">
-            <p>TextRPG</p>
-            <p>
+          <div id="project-block" className="text-left w-1/2 p-4 border border-white rounded-xl ml-5 mb-5">
+            <p className="pb-2 text-lg">TextRPG</p>
+            <p className="pb-2">
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur similique autem, laboriosam iste ad
               enim velit corporis molestiae commodi quibusdam.
             </p>
-            <button className="border-2 border-white p-1 w-20 rounded-xl hover:bg-gray-300">Repo</button>
-            <button className="border-2 border-white p-1 w-20 rounded-xl hover:bg-gray-300">Demo</button>
+            <button className="border-2 border-white p-1 w-20 rounded-xl hover:bg-zinc-950 mr-2">Repo</button>
+            <button className="border-2 border-white p-1 w-20 rounded-xl hover:bg-zinc-950">Demo</button>
           </div>
         </div>
 
-        <p className="text-center font-bold text-xl text-white pt-5">Rust</p>
-        <div className="flex flex-col text-white">
-          <p>hi</p>
+        <p className="text-center font-bold text-xl text-white pt-5 pb-5">Rust</p>
+        <div className="flex flex-col text-white justify-center items-center">
+          <div id="project-block" className="text-left w-1/2 p-4 border border-white rounded-xl ml-5 mb-5">
+            <p className="pb-2 text-lg">Temp Converter</p>
+            <p className="pb-2">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur similique autem, laboriosam iste ad
+              enim velit corporis molestiae commodi quibusdam.
+            </p>
+            <button className="border-2 border-white p-1 w-20 rounded-xl hover:bg-zinc-950 mr-2">Repo</button>
+            <button className="border-2 border-white p-1 w-20 rounded-xl hover:bg-zinc-950">Demo</button>
+          </div>
         </div>
       </div>
+
+      <footer className=" mt-20 bg-zinc-700 border-t border-white">
+        <div className="mx-auto max-w-screen-xl px-2 py-4 ">
+          <div className="flex items-center justify-center">
+            <p className="text-center text-sm text-gray-200">Farhan J Mohamed {new Date().getFullYear()}.</p>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
